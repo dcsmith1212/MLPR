0debb0d201a6fc89ab5918ab3c8ba0739ee437fd
dcsmith1212 <dcsmith121244@gmail.com>
2016-08-09 17:51:59 -0600

    Organized main.cpp, added to main3.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,3 +1,8 @@
+// g++ -std=c++11 main.cpp `pkg-config --cflags --libs opencv`
+/* OPTIMIZATION TO-DO LIST
+	* Convert all doubles to floats
+*/
+
 #include <iostream>
 #include <cmath>
 #include "opencv2/opencv.hpp"
@@ -9,59 +14,48 @@ using std::vector;
 
 using cv::Mat;
 
+// Function initializations (add //** to a personal CV library)
+std::vector<cv::Rect> find_bounding_boxes(cv::Mat image, double minAspectRatio, double minSolidity, double minAreaRatio); //**
+void display_bounding_box(cv::Mat image, cv::Rect boundingBox); //**
+cv::Mat pre_processing(cv::Mat rawFrame);
+
+// Main
 int main(int argc, char** argv) {
+	// "Global" variables and constants
+	Mat rawFrame;										// Holds raw input from the camera
+	cv::VideoCapture cap;								// Object that maintains camera/video
+	cv::namedWindow("Finalists", 0);					// Display and Mat for final output image
+	Mat finalistsDisplay;								// (will show boxed license plate)s
+	vector<cv::Rect> validBoundingBoxes;				// Holds final bounding boxes at each frame
+	const vector<int> IDEAL_WIDTHS = {1280, 820, 512};	// The raw frame will be resize to all these widths for processing
 
-/* OPTIMIZATION TO-DO LIST
-	* Convert all doubles to floats
-	* Consider linked lists or arrays instead of vectors
-*/
-// g++ -std=c++11 main.cpp `pkg-config --cflags --libs opencv`^C
-
-	Mat rawFrame;
-	Mat grayFrame;
-	Mat sobel1;
-	Mat sobel2;
-	cv::VideoCapture cap;
-
-	const int WHICH_IMAGE = 0;
-	if (WHICH_IMAGE == 0) { 
-		string filename = "yakvid.mp4";
+	// Sets the input type (image, .mp4, or camera feed)
+	const int IMAGE_TYPE = 0;
+	if (IMAGE_TYPE == 0) { 
+		string filename = "yakvid.mp4";		// Opens predefined video
 		cv::VideoCapture pac(filename);
 		cap = pac;
-	} else if (WHICH_IMAGE == 1) {
+	} else if (IMAGE_TYPE == 1) {
 		cout << "HEY" << endl;
-    	cv::VideoCapture pac(1); 		// open the default camera
-    	if(!pac.isOpened())  		// check if capture was successful
+    	cv::VideoCapture pac(1); 			// Opens the default camera
+    	if(!pac.isOpened())  				// Checks if capture was successful
         	return -1;
 		cap = pac;
-	} else if (WHICH_IMAGE == 3) {
-		rawFrame = cv::imread("yakxterra.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file
+	} else if (IMAGE_TYPE == 3) {			// Reads an image file
+		rawFrame = cv::imread("yakxterra.jpg", CV_LOAD_IMAGE_COLOR);
 
-		if(!rawFrame.data )                              // Check for invalid input
-		{
+		if(!rawFrame.data ) {				// Check for invalid input
 		    cout <<  "Could not open or find the image" << endl ;
 		    return -1;
 		}
 	}
-
-	cv::namedWindow("Finalists", 0);
-
-	const Mat SE1(8,  16, CV_8U, cv::Scalar(1));
-	const Mat SE2(40, 80, CV_8U, cv::Scalar(1));
-	const Mat SE3(15, 30, CV_8U, cv::Scalar(1));
-
 	
-	Mat finalistsDisplay;
-	Mat originalFrame;
-
-	
+	// Loops through video or passes image for plate recognition
     while (true) {
-		vector< vector<double> > validBoundingBoxes;
-	    if ((WHICH_IMAGE == 0) || (WHICH_IMAGE == 1)) cap >> rawFrame; 	// get a new frame from camera
+	    if ((IMAGE_TYPE == 0) || (IMAGE_TYPE == 1)) cap >> rawFrame; 	// Gets a new frame from camera
 
-		Mat newTest = rawFrame.clone();
-
-		if (WHICH_IMAGE == 1) {
+		// Increases the contrast of the video feed from USB camera
+		if (IMAGE_TYPE == 1) {
 			double alpha = 2;
 			double beta = -120;
 	 		for( int y = 0; y < rawFrame.rows; y++ ) { 
@@ -74,14 +68,14 @@ int main(int argc, char** argv) {
 			}
 		}
 
-
-		originalFrame = rawFrame.clone();
+		// Copies the raw frame for displaying boxed plate later
 		finalistsDisplay = rawFrame.clone();
 
-		const vector<int> IDEAL_WIDTHS = {1280, 820, 512};
-		double rawWidth = rawFrame.cols;	
+		// The processing is done for each of the sizes in IDEAL_WIDTHS
+		// This helps to generalize the range at which the plate location works
+		double rawWidth = rawFrame.cols;
 		for (int newWidth : IDEAL_WIDTHS) {
-			// Could define newWidth as constant in app
+			// Sets scale factor of image resize, so later all bounding boxes can be normalized back to original image
 			double scaleFactor;
 			if (rawWidth > newWidth) {
 				scaleFactor = (double)newWidth / rawWidth;
@@ -90,145 +84,70 @@ int main(int argc, char** argv) {
 			}			
 			else scaleFactor = 1;
 	
-			Mat frame(rawFrame.rows, rawFrame.cols, CV_32FC1);
-			rawFrame.convertTo(rawFrame, CV_32FC1, 1.0/255.0);		
+			// Runs frame through preprocessing
+			cv::Mat frame = pre_processing(rawFrame);
+			// Finds bounding boxes in preprocessed frame
+			vector<cv::Rect> boundingBoxes = find_bounding_boxes(frame, 0.98, 0.45, 2.7e-4);
 
-		    cv::cvtColor(rawFrame, grayFrame, CV_BGR2GRAY);
-			cv::Sobel(grayFrame, sobel1, -1, 1, 0, 3, 1.0/2.0);	
-			cv::Sobel(grayFrame, sobel2, -1, 0, 1, 3, 1.0/2.0);	
-			cv::magnitude(sobel1, sobel2, frame);
-			cv::namedWindow("FRM",0); cv::imshow("FRM", frame);
-			cv::morphologyEx(frame, frame, cv::MORPH_CLOSE, SE1);
-			cv::morphologyEx(frame, frame, cv::MORPH_TOPHAT, SE2);
-			cv::morphologyEx(frame, frame, cv::MORPH_OPEN, SE1);
-			cv::GaussianBlur(frame, frame, cv::Size(0,0), 3, 3);
-			frame = 255.0 * frame;
-			cv::threshold(frame, frame, 140, 255, cv::THRESH_BINARY);
-			cv::morphologyEx(frame, frame, cv::MORPH_OPEN, SE3);
-			cv::morphologyEx(frame, frame, cv::MORPH_DILATE, SE1);
-			frame.convertTo(frame, CV_8UC1, 1.0);	
-
-		    vector< vector<cv::Point> > contours;
-			cv::findContours(frame, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
-			Mat contoursImg = rawFrame.clone();
-
-			Mat bbImg = rawFrame.clone();
-			cv::Point pt1, pt2;
-			vector<cv::Rect> boundingBoxes;
-
-
-			double imageSize = frame.rows * frame.cols;
-			cv::Rect bbox;
-			double bbArea, areaRatio, aspectRatio;
-			bool validBox;
-			// Use range-based for loop or perhaps erase/delete idiom if speed is an issue
-			for (int i = 0; i < contours.size(); i++) {
-				bbox = cv::boundingRect(contours[i]);
-				bbArea = bbox.width * bbox.height;
-				areaRatio = bbArea / contourArea(contours[i]);
-				aspectRatio = bbox.width / bbox.height;
-				validBox = (areaRatio > 0.45) && (aspectRatio > 0.98) 
-										&& (bbArea / imageSize > 2.7e-4);
-				if (validBox)
-					boundingBoxes.push_back(bbox);
-			}
-
-
-			cv::Scalar color2(0,255,0);
+			// Variables used to subsample image with a valid bbox and then find its contours.
+			// If the subimage has >5 contours that could be plate numbers, it is sent on for further processing.
 			Mat subImage;
+			Mat graySub;
+			Mat binarySub;
 
-			for (int i = 0; i < boundingBoxes.size(); i++) {
-				cv::Point pt1, pt2;
-				pt1.x = boundingBoxes[i].x;
-				pt1.y = boundingBoxes[i].y;
-				pt2.x = boundingBoxes[i].x + boundingBoxes[i].width;
-				pt2.y = boundingBoxes[i].y + boundingBoxes[i].height;
+			int validityScore;
+			cv::Rect subBox;
+			double subAspectRatio;
+			cv::Rect validScaled;
 
-				cv::Rect testBb = boundingBoxes[i];
-				double x0 = testBb.x;
-				double y0 = testBb.y;
-				double w = testBb.width;
-				double h = testBb.height;
-			
-				cv::Mat graySub;
-				cv::Mat binarySub;
-
-				subImage = rawFrame(boundingBoxes[i]);	
+			for ( cv::Rect bbox : boundingBoxes ) {
+				// Subsamples image at a valid bounding box and converts to binary using Otsu threshold
+				subImage = rawFrame(bbox);	
 				cv::cvtColor(subImage, graySub, CV_BGR2GRAY);
 				graySub.convertTo(graySub, CV_8UC1, 1.0);
-				
 				cv::threshold(graySub, binarySub, 0, 255, CV_THRESH_BINARY_INV + CV_THRESH_OTSU);
 
-				Mat binarySub_CLONE = binarySub.clone();
-			
+				// Find contours of binarized subimage
+				Mat binarySubClone = binarySub.clone();
 				vector< vector<cv::Point> > subContours;
-				cv::findContours(binarySub_CLONE, subContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);	
+				cv::findContours(binarySubClone, subContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);	// Consider changing mode
 
-				// Attempt at a simpler filtering algorithm than the one used with matlab
-				int validityScore = 0;
+				// Attempt at a simpler filtering algorithm than the one used with matlab (the Russian version)
+				validityScore = 0;
 				for (int k = 0; k < subContours.size(); k++) {
-					cv::Rect subBox = cv::boundingRect(subContours[k]);
-					double subAspectRatio = (double)subBox.width / (double)subBox.height;	
+					subBox = cv::boundingRect(subContours[k]);
+					subAspectRatio = (double)subBox.width / (double)subBox.height;	
 
 					if (subAspectRatio >= 0.3 && subAspectRatio <= 0.5) validityScore++;
 				}
 			
+				// If subimage has more than 5 blobs whose aspect ratios match those for plate numbers,
+				// consider the subimage valid
 				if (validityScore > 5) {
-					double x0 = boundingBoxes[i].x / scaleFactor;
-					double y0 = boundingBoxes[i].y / scaleFactor;
-					double w = boundingBoxes[i].width / scaleFactor;
-					double h = boundingBoxes[i].height / scaleFactor;
-					vector<double> validScaled = {x0, y0, w, h};
+					validScaled.x = bbox.x / scaleFactor;
+					validScaled.y = bbox.y / scaleFactor;
+					validScaled.width  = bbox.width  / scaleFactor;
+					validScaled.height = bbox.height / scaleFactor;
 					validBoundingBoxes.push_back(validScaled);
 				}
 			}
 		}
 
+		// TODO: Currently the plate may be capture by more than one of the size.
+		// Find a quick way to take the biggest bounding box around the plate if this happens.
 
-		cv::Point pt3, pt4;
-		if (!validBoundingBoxes.empty()) {	
-			std::sort(validBoundingBoxes.begin(), validBoundingBoxes.end(), 
-				[](const vector<double>& vec1, const vector<double>& vec2) {return vec1[0] < vec2[0];} );
-			
-			double maxArea = validBoundingBoxes[0][2] * validBoundingBoxes[0][3];
-			int maxIndex   = 0;
-
-			for (int i = 1; i < validBoundingBoxes.size(); i++) {
-				cv::Vec<double,2> prevPt, currPt;
-				currPt[0] = validBoundingBoxes[i][0];
-				currPt[1] = validBoundingBoxes[i][1];
-				prevPt[0] = validBoundingBoxes[i-1][0];
-				prevPt[1] = validBoundingBoxes[i-1][1];
-
-				if ( norm(prevPt - currPt) > (double)originalFrame.rows / 6.0 ) {	// Un-harcode this!!
-					pt3.x = validBoundingBoxes[maxIndex][0];
-					pt3.y = validBoundingBoxes[maxIndex][1];
-					pt4.x = validBoundingBoxes[maxIndex][0] + validBoundingBoxes[maxIndex][2];
-					pt4.y = validBoundingBoxes[maxIndex][1] + validBoundingBoxes[maxIndex][3];
-					cv::rectangle(finalistsDisplay, pt3, pt4, CV_RGB(0,0,255), 7);
-
-					maxArea = validBoundingBoxes[i][2] * validBoundingBoxes[i][3];
-					maxIndex = i;
-				}
-				else {
-					if ( (double)validBoundingBoxes[i][2] * (double)validBoundingBoxes[i][3] > maxArea ) {
-						maxArea = (double)validBoundingBoxes[i][2] * (double)validBoundingBoxes[i][3];
-						maxIndex = i;
-					}
-				}
-			}
-
-			pt3.x = validBoundingBoxes[maxIndex][0];
-			pt3.y = validBoundingBoxes[maxIndex][1];
-			pt4.x = validBoundingBoxes[maxIndex][0] + validBoundingBoxes[maxIndex][2];
-			pt4.y = validBoundingBoxes[maxIndex][1] + validBoundingBoxes[maxIndex][3];
-			cv::rectangle(finalistsDisplay, pt3, pt4, CV_RGB(0,0,255), 7);
+		// Displays all the valid bounding boxes on the original image
+		if (!validBoundingBoxes.empty()) {				
+			for ( cv::Rect bbox : validBoundingBoxes ) 
+				display_bounding_box(finalistsDisplay, bbox);
 		}
-
 		cv::imshow("Finalists", finalistsDisplay);
 
-		if (WHICH_IMAGE == 3) {
+		// Clears vector for use with next frame
+		validBoundingBoxes.clear();
+
+		// Breaks loop if input is just an image
+		if (IMAGE_TYPE == 3) {
 			cv::waitKey(0);
 			break;
 		}
@@ -237,3 +156,82 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+//-----------------------------------------------------------------------------
+// Functions
+
+// Generalize this function by adding option to only store bboxes with certain area,
+// by adding option to display bboxes
+std::vector<cv::Rect> find_bounding_boxes(cv::Mat image, double minAspectRatio, double minSolidity, double minAreaRatio) {
+	// Finds the contours of the given image
+	std::vector< std::vector< cv::Point >> contours;
+	cv::findContours(image, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+	
+	// Finds the bounding box of each contour
+	std:vector<cv::Rect> boundingBoxes;
+	cv::Rect bbox;
+	double bbArea, solidity, aspectRatio, imageSize, areaRatio;
+	bool validBox;
+	for ( std::vector<cv::Point> contour : contours ) {
+		bbox = cv::boundingRect(contour);
+
+		// Only returns the bounding box if it fits the given criteria
+		bbArea      = (double)bbox.width * (double)bbox.height;
+		solidity    = bbArea / (double)contourArea(contour);
+		aspectRatio = bbox.width / bbox.height;
+		imageSize   = image.rows * image.cols;
+		areaRatio   = bbArea / imageSize;
+		validBox    = (solidity > minSolidity) && (aspectRatio > minAspectRatio) 
+								&& (areaRatio > minAreaRatio);
+		if (validBox) boundingBoxes.push_back(bbox);
+	}
+
+	return boundingBoxes;
+}
+
+
+// Displays the given bounding box on the given image
+void display_bounding_box(cv::Mat image, cv::Rect bbox) {
+	// Must pass rectangle function the upper-left and lower-right points of bounding box
+	cv::Point pt1, pt2;
+	pt1.x = bbox.x;
+	pt1.y = bbox.y;
+	pt2.x = bbox.x + bbox.width;
+	pt2.y = bbox.y + bbox.height;
+	cv::rectangle(image, pt1, pt2, CV_RGB(0,0,255), 7);
+}
+
+
+// Filters the video frame before converting to a binary form which (ideally) highlights the plate
+cv::Mat pre_processing(cv::Mat rawFrame) {
+	cv::Mat grayFrame;
+	cv::Mat sobel1;
+	cv::Mat sobel2;
+
+	// Structuring elements for closing, opening, and tophat
+	cv::Mat strel1(8,  16, CV_8U, cv::Scalar(1));
+	cv::Mat strel2(40, 80, CV_8U, cv::Scalar(1));
+	cv::Mat strel3(15, 30, CV_8U, cv::Scalar(1));
+
+	// Scales the image down for processing
+	cv::Mat processedFrame(rawFrame.rows, rawFrame.cols, CV_32FC1);
+	rawFrame.convertTo(rawFrame, CV_32FC1, 1.0/255.0);		
+
+    cv::cvtColor(rawFrame, grayFrame, CV_BGR2GRAY);
+	cv::Sobel(grayFrame, sobel1, -1, 1, 0, 3, 1.0/2.0);	
+	cv::Sobel(grayFrame, sobel2, -1, 0, 1, 3, 1.0/2.0);	
+	cv::magnitude(sobel1, sobel2, processedFrame);
+	cv::morphologyEx(processedFrame, processedFrame, cv::MORPH_CLOSE, strel1);
+	cv::morphologyEx(processedFrame, processedFrame, cv::MORPH_TOPHAT, strel2);
+	cv::morphologyEx(processedFrame, processedFrame, cv::MORPH_OPEN, strel1);
+	cv::GaussianBlur(processedFrame, processedFrame, cv::Size(0,0), 3, 3);
+	processedFrame = 255.0 * processedFrame;
+	cv::threshold(processedFrame, processedFrame, 140, 255, cv::THRESH_BINARY);
+	cv::morphologyEx(processedFrame, processedFrame, cv::MORPH_OPEN, strel3);
+	cv::morphologyEx(processedFrame, processedFrame, cv::MORPH_DILATE, strel1);
+	processedFrame.convertTo(processedFrame, CV_8UC1, 1.0);
+
+	return processedFrame;
+}
+
+
--- c++/main3.cpp
+++ c++/main3.cpp
@@ -1,3 +1,5 @@
+// g++ -std=c++11 main3.cpp `pkg-config --cflags --libs opencv`
+
 #include <iostream>
 #include <cmath>
 #include "opencv2/opencv.hpp"
@@ -11,6 +13,7 @@ using cv::Mat;
 using cv::Point;
 using cv::threshold;
 using cv::findContours;
+using cv::drawContours;
 using cv::namedWindow;
 using cv::imshow;
 using cv::imread;
@@ -21,6 +24,7 @@ using cv::waitKey;
 using cv::cvtColor;
 
 void showim(string, cv::Mat);
+std::vector<cv::Rect> findBoundingBoxes(cv::Mat image);
 
 int main(int argc, char** argv) {
 	Mat rawFrame = imread("yakxterra.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file
@@ -31,12 +35,68 @@ int main(int argc, char** argv) {
 	    return -1;
 	}
 
-	Mat grayFrame, binary;
+	Mat grayFrame, blurred, binary;
 	cvtColor(rawFrame, grayFrame, CV_BGR2GRAY);
+	cv::GaussianBlur(grayFrame, blurred, cv::Size(0,0), 3, 3);
+	showim("Gray", blurred);
 
-	showim("Gray", grayFrame);
+	Mat bbImg = rawFrame.clone();
+	namedWindow("Boxes", 0);
+
+	vector<cv::Rect> validBoundingBoxes;
+
+
+	int minArea = 1500;
+	int maxArea = 5000;
+	// This loop iterates of thresh values, finding the bounding boxes for each resulting image that lie within the area and aspect ration constraints.
 	for (int thresh = 10; thresh <= 220; thresh+=10) {
+		threshold(grayFrame, binary, thresh, 255, cv::THRESH_BINARY_INV);
+		vector<cv::Rect> boundingBoxes = findBoundingBoxes(binary);
+
+		// This will all be generalized in the above function
+		cv::Point pt1, pt2;
+		for (int i = 0; i < boundingBoxes.size(); i++) {
+			cv::Rect bbox = boundingBoxes[i];
+			pt1.x = bbox.x;
+			pt1.y = bbox.y;
+			pt2.x = bbox.x + bbox.width;
+			pt2.y = bbox.y + bbox.height;
+
+			double bbArea = (double)bbox.width * (double)bbox.height;
+			double bbRatio = (double)bbox.width / (double)bbox.height;
+
+			// Relate these tolerances back to the image size for generality
+			if ((bbArea > minArea) && (bbArea < maxArea) && (bbRatio > 0.3) && (bbRatio < 0.5)) {
+				cv::rectangle(bbImg, pt1, pt2, CV_RGB(0,255,0), 4);
+				validBoundingBoxes.push_back(bbox);
+			}
+		}
 	}
+
+	imshow("Boxes", bbImg);
+
+
+	int nAreaBins = 30;
+	int xBins = 15;
+	int yBins = 20;
+
+	double areaBinSize = (double)(maxArea - minArea) / (double)nAreaBins;
+	vector<int> areaHist(nAreaBins);
+	// This code loops through the valid bounding boxes and separates them into a 3D histogram based on x location, y lcoation, and area.
+
+	int areaIndx;
+	double bbArea;
+	for ( cv::Rect bbox : validBoundingBoxes ) {
+		bbArea = (double)bbox.width * (double)bbox.height;
+		areaIndx = (int)((bbArea - minArea) / areaBinSize);
+		areaHist[areaIndx] += 1;
+	}
+
+	
+	for ( int i : areaHist )
+		cout << i << endl;	
+
+
 
 	waitKey(0);
 	return 0;
@@ -47,4 +107,32 @@ void showim(string windowName, cv::Mat image) {
 	cv::namedWindow(windowName, 0);
 	cv::imshow(windowName, image);
 }
+
+
+// Generalize this function by adding option to only store bboxes with certain area,
+// by adding option to display bboxes
+std::vector<cv::Rect> findBoundingBoxes(cv::Mat image) {
+	std::vector< std::vector< cv::Point >> contours;
+	cv::findContours(image, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+	
+	cv::Point pt1, pt2;
+	std:vector<cv::Rect> boundingBoxes;
+	cv::Rect bbox;
+
+	for ( std::vector<cv::Point> contour : contours ) {
+		bbox = cv::boundingRect(contour);
+		boundingBoxes.push_back(bbox);
+	}
+
+	return boundingBoxes;
+} 
+
+
+
+
+
+
+
+
+
 
